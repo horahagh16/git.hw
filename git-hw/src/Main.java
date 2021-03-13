2d2d6a567e88ba1fdb4825f718fb9ee7a28c40ca
Hora Haghighatkhah <79784901+horahagh16@users.noreply.github.com>
2021-03-13 19:24:54 +0330

    Add files via upload

---
+++ git-hw/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("My name is: Hora Haghighatkhah");
+        System.out.println("My student ID is 99243032");
+    }
+}
